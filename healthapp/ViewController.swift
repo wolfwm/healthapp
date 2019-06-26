bb2917b519369a2a1d2b19e538b2eb670d5f48f0
Wolfgang Walder <wolfwalder@gmail.com>
2019-06-26 20:00:42 -0300

    Initialized Xcode project

---
+++ healthapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  healthapp
+//
+//  Created by Wolfgang Walder on 26/06/19.
+//  Copyright © 2019 Wolfgang Walder. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
